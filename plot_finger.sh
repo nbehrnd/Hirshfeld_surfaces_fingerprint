667f4083cab0aba4a7b4ac932990262f31c19b0d
Norwid Behrnd <nbehrnd@yahoo.com>
2019-11-02 17:28:01 +0100

    Re-enable function of the two bash scripts in Linux
    Both bash scripts contained the instruction
    
    sp'.fprnt'
    
    which hindered the successful deployment in Linux Xubuntu 18.04.3 LTS
    with gnuplot 5.2.7.  This commit removes this roadblock.

--- plot_finger.sh
+++ plot_finger.sh
@@ -6,4 +6,4 @@ gnuplot  -e "set term png size 4096,4096 font \"Arial,64\" enha lw 10; set outpu
                    3  0.5 1.0 0.5, \
                    4  1.0 1.0 0.0, \
                    5  1.0 0.5 0.0, \
-                   6  1.0 0.0 0.0 ); set grid lw 0.25; set size square ; set xtics format \"%3.1f\"; set ytics format \"%3.1f\"; set xtics 0.4,0.2 ; set ytics 0.4,0.2 ; set g ;set cbrange [0:0.08]; sp'$1.fprnt' u 1:2:3  w p pt 5 lc palette z"
+                   6  1.0 0.0 0.0 ); set grid lw 0.25; set size square ; set xtics format \"%3.1f\"; set ytics format \"%3.1f\"; set xtics 0.4,0.2 ; set ytics 0.4,0.2 ; set g ;set cbrange [0:0.08]; sp'$1' u 1:2:3  w p pt 5 lc palette z"
