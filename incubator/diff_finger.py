af164caa8888f7dfe18014e049588f27cebaa80a
Norwid Behrnd <nbehrnd@yahoo.com>
2025-02-26 13:22:00 +0100

    refactor(diff_finger.py): start using argparse
    To modernize the script, it is rewritten.  This starts by the using
    argparse.  This commit mutes most of the script's functionality.
    
    Signed-off-by: Norwid Behrnd <nbehrnd@yahoo.com>

--- incubator/diff_finger.py
+++ incubator/diff_finger.py
@@ -4,170 +4,205 @@
 # author:  nbehrnd@yahoo.com
 # license: 2019, GPLv2
 # date:    2019-12-19 (YYYY-MM-DD)
-# edit:    [2024-12-13 Fri]
-""" Compute difference maps of normalized 2D Hirshfeld surface maps
+# edit:    [2025-02-26 Wed]
+"""Compute difference maps of normalized 2D Hirshfeld surface maps
 
-    The number of programming languages around the computation of already
-    normalized 2D Hirshfeld surface maps and difference Hirshfeld surface
-    maps may be considered as higher, than necessary.  Potentially, their
-    number may be lowered.  There already is one moderator script written
-    in CPython, i.e., Hirshfeld_moderator.py, suggesting to continue with
-    this language, too.
+The number of programming languages around the computation of already
+normalized 2D Hirshfeld surface maps and difference Hirshfeld surface
+maps may be considered as higher, than necessary.  Potentially, their
+number may be lowered.  There already is one moderator script written
+in CPython, i.e., Hirshfeld_moderator.py, suggesting to continue with
+this language, too.
 
-    This script serves as a proof-of-concept for the comparison of two 2D
-    Hirshfeld surface fingerprint maps (by fingerprint.f90)
-    in a round-Robin tournament.  It probes the two .dat files subject to
-    comparison match in terms of map ranges de/di: both the number of
-    entries (lines) must be equal, as the lowest y_value.  This allows to
-    probe standard, translated, or extended map range, respectively.
+This script serves as a proof-of-concept for the comparison of two 2D
+Hirshfeld surface fingerprint maps (by fingerprint.f90)
+in a round-Robin tournament.  It probes the two .dat files subject to
+comparison match in terms of map ranges de/di: both the number of
+entries (lines) must be equal, as the lowest y_value.  This allows to
+probe standard, translated, or extended map range, respectively.
 
-    To work with, place the script in the directory of (then already
-    normalized) .dat files.  It is launched from the CLI by
+To work with, place the script in the directory of (then already
+normalized) .dat files.  It is launched from the CLI by
 
-    python3 diff_finger.py
+python3 diff_finger.py
 
-    This script diff_finger.py still is independent to the actions by
-    hirshfeld_moderator.py.  It is neither called, nor are its results
-    explicitly used by hirshfeld_moderator.  This version relies on third
-    party numpy at version 2.1.0 or higher and is known to process with
-    Python 3.12.7 and numpy 2.2.0 (fetched via `requirements.txt` from PyPI)
-    in Linux Debian 13/trixie."""
+This script diff_finger.py still is independent to the actions by
+hirshfeld_moderator.py.  It is neither called, nor are its results
+explicitly used by hirshfeld_moderator.  This version relies on third
+party numpy at version 2.1.0 or higher and is known to process with
+Python 3.12.7 and numpy 2.2.0 (fetched via `requirements.txt` from PyPI)
+in Linux Debian 13/trixie."""
 
+import argparse
 import fnmatch
 import os
 import sys
 
 import numpy as np
 
-diff_register = []
 
-# identification of the files to work with:
-for file in os.listdir("."):
-    if fnmatch.fnmatch(file, "*.dat") and (
-        fnmatch.fnmatch(file, "*diff*.dat") is False
-    ):
+def get_args():
+    """collect the command line arguments"""
+    parser = argparse.ArgumentParser(
+        description="""
+With two Hirschfeld maps A (provided by file `A.dat`) and B (file `B.dat`), a
+difference map can be computed; the script writes the result of this into file
+`diff_A_B.dat`.  More generally, for a list of maps e.g., A, B, C, D the script
+launches a round-Robin approach to check all permutations (AB, AC, AD, BC, BD,
+CD) and to store the results accordingly.""",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
 
-        diff_register.append(file)
-diff_register.sort()
+    parser.add_argument(
+        "file",
+        help="provide two, or more Hirshfeld map files to process",
+        metavar="FILE",
+        type=argparse.FileType("rt"),
+        nargs="+",
+    )
 
-# comparing the normalized 2D Hirshfeld surface maps
-while len(diff_register) > 1:
-    for entry in diff_register[1:]:
-        ref_file = diff_register[0]
-        probe_file = entry
-        print(f"Comparing {ref_file} with {probe_file}.")
+    return parser.parse_args()
 
-        # consistency check for de/di
-        ref_screen = []
-        with open(ref_file, mode="r") as ref_source:
-            for line in ref_source:
-                ref_screen.append(str(line.strip()))
-        ref_y_min = str(ref_screen[0].split()[1])[:4]
 
-        probe_screen = []
-        with open(probe_file, mode="r") as probe_source:
-            for line in probe_source:
-                probe_screen.append(str(line.strip()))
-        probe_y_min = str(probe_screen[0].split()[1])[:4]
+def main():
+    """join the functionalities"""
+    args = get_args()
+    list_of_files = args.file
+    print([data_file.name for data_file in list_of_files])
 
-        if (len(ref_screen) == len(probe_screen)) and (ref_y_min == probe_y_min):
-            pass
-        else:
-            continue
 
-        # branch about the reference file:
-        content_ref_file = []
-        with open(ref_file, mode="r", encoding="utf-8") as source_ref:
-            for line in source_ref:
-                trimmed_line = str(line).strip()  # remove line feed
+# diff_register = []
 
-                split = trimmed_line.split()
-                # branch about lines just prior to y-reset:
-                if len(split) is None:
-                    pass
-                # branch about lines 'with visible entries':
-                if len(split) == 3:
-                    retain = split
-                    content_ref_file.append(retain)
+## identification of the files to work with:
+# for file in os.listdir("."):
+#    if fnmatch.fnmatch(file, "*.dat") and (
+#        fnmatch.fnmatch(file, "*diff*.dat") is False
+#    ):
 
-        # convert the list into an array, treat entries as floats
-        ref_array = np.array(content_ref_file)
-        ref_array = ref_array.astype(float)
+#        diff_register.append(file)
+# diff_register.sort()
 
-        # branch about the probe file
-        content_probe_file = []
-        with open(probe_file, mode="r", encoding="utf-8") as source_probe:
-            for line2 in source_probe:
-                trimmed_line2 = str(line2).strip()  # remove line feed
+## comparing the normalized 2D Hirshfeld surface maps
+# while len(diff_register) > 1:
+#    for entry in diff_register[1:]:
+#        ref_file = diff_register[0]
+#        probe_file = entry
+#        print(f"Comparing {ref_file} with {probe_file}.")
 
-                split2 = trimmed_line2.split()
-                # branch about lines just prior to y-reset:
-                if len(split2) is None:
-                    pass
-                # branch about lines 'with visible entries':
-                if len(split2) == 3:
-                    retain2 = split2
-                    content_probe_file.append(retain2)
+#        # consistency check for de/di
+#        ref_screen = []
+#        with open(ref_file, mode="r") as ref_source:
+#            for line in ref_source:
+#                ref_screen.append(str(line.strip()))
+#        ref_y_min = str(ref_screen[0].split()[1])[:4]
 
-        # convert the list into an array, treat entries as floats
-        probe_array = np.array(content_probe_file)
-        probe_array = probe_array.astype(float)
+#        probe_screen = []
+#        with open(probe_file, mode="r") as probe_source:
+#            for line in probe_source:
+#                probe_screen.append(str(line.strip()))
+#        probe_y_min = str(probe_screen[0].split()[1])[:4]
 
-        # work at level of the matrix-like arrays
-        # construct an array of the first two columns of the ref_array
-        coordinates_array = ref_array
-        coordinates_array = np.delete(coordinates_array, 2, axis=1)
+#        if (len(ref_screen) == len(probe_screen)) and (ref_y_min == probe_y_min):
+#            pass
+#        else:
+#            continue
 
-        # subtract z-values of probe_file from z-values of ref_file;
-        # prior to this, remove 'x-' and 'y-coordinate column'
-        z_probe_array = np.delete(probe_array, 0, axis=1)
-        z_probe_array = np.delete(z_probe_array, 0, axis=1)
+#        # branch about the reference file:
+#        content_ref_file = []
+#        with open(ref_file, mode="r", encoding="utf-8") as source_ref:
+#            for line in source_ref:
+#                trimmed_line = str(line).strip()  # remove line feed
 
-        z_ref_array = np.delete(ref_array, 0, axis=1)
-        z_ref_array = np.delete(z_ref_array, 0, axis=1)
+#                split = trimmed_line.split()
+#                # branch about lines just prior to y-reset:
+#                if len(split) is None:
+#                    pass
+#                # branch about lines 'with visible entries':
+#                if len(split) == 3:
+#                    retain = split
+#                    content_ref_file.append(retain)
 
-        diff_array = z_ref_array - z_probe_array
+#        # convert the list into an array, treat entries as floats
+#        ref_array = np.array(content_ref_file)
+#        ref_array = ref_array.astype(float)
 
-        # append diff_array to the coordinates_array:
-        result = np.append(coordinates_array, diff_array, axis=1)
+#        # branch about the probe file
+#        content_probe_file = []
+#        with open(probe_file, mode="r", encoding="utf-8") as source_probe:
+#            for line2 in source_probe:
+#                trimmed_line2 = str(line2).strip()  # remove line feed
 
-        # deposit a permanent record of results by numpy 'as-such'
-        # This lacks the linefeed to be re-inserted, and often carries
-        # many more decimals, than wished.
-        # np.savetxt("result_subtraction.csv", result)
+#                split2 = trimmed_line2.split()
+#                # branch about lines just prior to y-reset:
+#                if len(split2) is None:
+#                    pass
+#                # branch about lines 'with visible entries':
+#                if len(split2) == 3:
+#                    retain2 = split2
+#                    content_probe_file.append(retain2)
 
-        # return from array to list level, start a moderated formatting
-        result_list = result.tolist()
+#        # convert the list into an array, treat entries as floats
+#        probe_array = np.array(content_probe_file)
+#        probe_array = probe_array.astype(float)
 
-        output = str("diff_") + str(ref_file)[:-4] + str("_") + str(probe_file)
+#        # work at level of the matrix-like arrays
+#        # construct an array of the first two columns of the ref_array
+#        coordinates_array = ref_array
+#        coordinates_array = np.delete(coordinates_array, 2, axis=1)
 
-        with open(output, mode="w", encoding="utf-8") as newfile:
-            for result_entry in result_list:
-                to_reformat = str(result_entry).split()
+#        # subtract z-values of probe_file from z-values of ref_file;
+#        # prior to this, remove 'x-' and 'y-coordinate column'
+#        z_probe_array = np.delete(probe_array, 0, axis=1)
+#        z_probe_array = np.delete(z_probe_array, 0, axis=1)
 
-                x_value = str("{:3.2f}".format(float(str(to_reformat[0])[1:-1])))
-                y_value = str("{:3.2f}".format(float(str(to_reformat[1])[0:-1])))
-                z_value = str(
-                    "{:9.6f}".format(round(float(str(to_reformat[2])[0:-1]), 8))
-                )
+#        z_ref_array = np.delete(ref_array, 0, axis=1)
+#        z_ref_array = np.delete(z_ref_array, 0, axis=1)
 
-                # re-insert the blanks met in normalized 2D fingerprints:
-                # if float(y_value) == float(ref_y_min):
-                    # newfile.write("\n")
+#        diff_array = z_ref_array - z_probe_array
 
-                retain = str(f"{x_value} {y_value} {z_value}\n")
-                newfile.write(retain)
+#        # append diff_array to the coordinates_array:
+#        result = np.append(coordinates_array, diff_array, axis=1)
 
-        # Remove the very first line in the report file (a blank one):
-        interim = []
-        with open(output, mode="r", encoding="utf-8") as source:
-            for line in source:
-                interim.append(line)
-        with open(output, mode="w", encoding="utf-8") as newfile:
-            for entry in interim[1:]:
-                newfile.write(str(entry))
+#        # deposit a permanent record of results by numpy 'as-such'
+#        # This lacks the linefeed to be re-inserted, and often carries
+#        # many more decimals, than wished.
+#        # np.savetxt("result_subtraction.csv", result)
 
-    # enter the next round of the Round robin tournament:
-    del diff_register[0]
-print("done")
-sys.exit(0)
+#        # return from array to list level, start a moderated formatting
+#        result_list = result.tolist()
+
+#        output = str("diff_") + str(ref_file)[:-4] + str("_") + str(probe_file)
+
+#        with open(output, mode="w", encoding="utf-8") as newfile:
+#            for result_entry in result_list:
+#                to_reformat = str(result_entry).split()
+
+#                x_value = str("{:3.2f}".format(float(str(to_reformat[0])[1:-1])))
+#                y_value = str("{:3.2f}".format(float(str(to_reformat[1])[0:-1])))
+#                z_value = str(
+#                    "{:9.6f}".format(round(float(str(to_reformat[2])[0:-1]), 8))
+#                )
+
+#                # re-insert the blanks met in normalized 2D fingerprints:
+#                # if float(y_value) == float(ref_y_min):
+#                    # newfile.write("\n")
+
+#                retain = str(f"{x_value} {y_value} {z_value}\n")
+#                newfile.write(retain)
+
+#        # Remove the very first line in the report file (a blank one):
+#        interim = []
+#        with open(output, mode="r", encoding="utf-8") as source:
+#            for line in source:
+#                interim.append(line)
+#        with open(output, mode="w", encoding="utf-8") as newfile:
+#            for entry in interim[1:]:
+#                newfile.write(str(entry))
+
+#    # enter the next round of the Round robin tournament:
+#    del diff_register[0]
+# print("done")
+# sys.exit(0)
+
+if __name__ == "__main__":
+    main()
